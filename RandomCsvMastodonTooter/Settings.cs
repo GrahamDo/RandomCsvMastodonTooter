a145149717e53a0fb1ac334618230dd280bf4ae1
GrahamDo <downs_graham@yahoo.com>
2025-09-05 16:38:02 +0200

    Implement Settings.Load() method

--- RandomCsvMastodonTooter/Settings.cs
+++ RandomCsvMastodonTooter/Settings.cs
@@ -1,12 +1,21 @@
+using Newtonsoft.Json;
+
 namespace RandomCsvMastodonTooter;
 
 public class Settings
 {
+    private const string SettingsFileName = "settings.json";
+    
     public string InstanceUrl { get; set; } = string.Empty;
     public string Token { get; set; } = string.Empty;
     
     public static Settings Load()
     {
-        throw new NotImplementedException();
+        if (!File.Exists(SettingsFileName))
+            return new Settings();
+
+        var text = File.ReadAllText(SettingsFileName);
+        return JsonConvert.DeserializeObject<Settings>(text) ??
+               throw new ApplicationException($"Your '{SettingsFileName}' appears to be empty or corrupt.");
     }
 }
